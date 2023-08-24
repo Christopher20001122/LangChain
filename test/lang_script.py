1f98ce2b7b37d17c69e5ed56cbb5537410a9b30f
Christopher20001122 <christopherismaelpalloarias@gmail.com>
2023-08-23 21:50:12 -0500

    Mejora Interfaz

--- test/lang_script.py
+++ test/lang_script.py
@@ -6,8 +6,9 @@ from langchain.llms import OpenAI
 from langchain.document_loaders import PyPDFLoader
 import streamlit as st
 
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = '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'
 default_doc_name = 'doc.pdf'
+
 
 def process_doc(
         path: str = 'https://proceedings.neurips.cc/paper_files/paper/2017/file/3f5ee243547dee91fbd053c1c4a845aa-Paper.pdf',
@@ -18,40 +19,51 @@ def process_doc(
         else PyPDFLoader(path)
 
     doc = loader.load_and_split()
-
-    print(doc[-1])
-
     db = Chroma.from_documents(doc, embedding=OpenAIEmbeddings())
-
     qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type='stuff', retriever=db.as_retriever())
+    return qa.run(question)
 
-    st.write(qa.run(question))
-    #print(qa.run(question))
 
-def cliente():
+def main():
+    st.set_page_config(page_title='Gestionar LLM con LangChain', layout='wide')
+
+    st.image('https://aka-cdn.uce.edu.ec/ares/tmp/logo3.png', width=150)
     st.title('Manage LLM with LangChain')
-    uploader = st.file_uploader('Upload PDF', type='pdf')
 
-    if uploader:
-        with open(f'./{default_doc_name}', 'wb') as f:
-            f.write(uploader.getbuffer())
-        st.success('PDF saved!!')
+    col1, col2 = st.columns(2)
 
-    question = st.text_input('Generar un resumen de 20 palabras sobre el pdf',
-                             placeholder='Give response about your PDF', disabled=not uploader)
+    # Subir PDF
+    with col1:
+        st.markdown('## Subir PDF')
+        uploader = st.file_uploader('Selecciona un archivo PDF', type='pdf', key='uploader')
+        uploaded = False
 
-    if st.button('Send Question'):
         if uploader:
-            process_doc(
-                path=default_doc_name,
-                is_local=True,
-                question=question
-            )
+            with open(f'./{default_doc_name}', 'wb') as f:
+                f.write(uploader.getbuffer())
+            uploaded = True
+            st.success('PDF cargado exitosamente!')
+
+    # Pregunta
+    with col2:
+        st.markdown('## Hacer una Pregunta')
+        if not uploaded:
+            st.warning('Por favor, carga un PDF primero.')
         else:
-            st.info('Loading default PDF')
-            process_doc()
+            question = st.text_input('Escribe una pregunta sobre el PDF cargado',
+                                     placeholder='Escribe aquí tu pregunta', key='question')
+            if st.button('Enviar Pregunta', key='send_question'):
+                with st.spinner('Procesando pregunta...'):
+                    result = process_doc(
+                        path=default_doc_name,
+                        is_local=True,
+                        question=question
+                    )
+                    st.success('Pregunta procesada!')
+                    st.write('Respuesta:', result)
+
 
 if __name__ == '__main__':
-    cliente()
+    main()
 
 #streamlit run test/lang_script.py
